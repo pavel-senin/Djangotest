2167e9d47611c25f7fee450f3d45dcbfec89e62f
Pavel <pasha-senin@mail.ru>
2024-05-30 02:53:16 +0300

    to get ready for python anywhere

--- dbproject/dbproject/settings.py
+++ dbproject/dbproject/settings.py
@@ -42,10 +42,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'debug_toolbar',
     'dbapp',
 ]
 
 MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
